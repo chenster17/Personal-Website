ec5d2b9c626a625fef0cd380bf7b9071b94a887f
Zhao <Zhao@192.168.1.10>
2016-08-09 22:17:08 -0400

    adding progress bar

---
+++ js/CustomJS.js
@@ -0,0 +1,15 @@
+function move(temp) {
+  var vtemp = temp
+  var elem = document.getElementById("myBar");
+  var width = 10;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= vtemp) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      document.getElementById("label").innerHTML = width * 1  + '%';
+    }
+  }
+}
